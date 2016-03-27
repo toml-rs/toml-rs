568dd2ef4ba7db91d59988825f07e3e847e8c8d8
Bourgond Aries <macocio@gmail.com>
2016-03-27 21:19:03 +0200

    Fix the indentation to be consistent with the repository

--- src/lib.rs
+++ src/lib.rs
@@ -445,12 +445,12 @@ mod tests {
         assert_eq!(*looked, Value::Integer(0));
     }
 
-		#[test]
-		fn lookup_advanced_table() {
+    #[test]
+    fn lookup_advanced_table() {
         let value: Value = r#"[table."name.other"] value = "my value""#.parse().unwrap();
         let looked = value.lookup(r#"table."name.other".value"#).unwrap();
         assert_eq!(*looked, Value::String(String::from("my value")));
-		}
+    }
 
     #[test]
     fn lookup_mut_advanced() {
