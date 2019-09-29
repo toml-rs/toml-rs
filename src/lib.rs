550b139cf4355026f04558b7bff789f63dd45c93
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2019-09-29 13:19:51 +0200

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -147,7 +147,6 @@
 #![doc(html_root_url = "https://docs.rs/toml/0.5")]
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-
 // Makes rustc abort compilation if there are any unsafe blocks in the crate.
 // Presence of this annotation is picked up by tools such as cargo-geiger
 // and lets them ensure that there is indeed no unsafe code as opposed to
