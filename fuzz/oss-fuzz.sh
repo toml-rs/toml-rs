8e2b58bec924eb710b252f4f635c3a09e024cf98
Evgeny Vereshchagin <evvers@ya.ru>
2021-05-31 17:05:07 +0000

    download a dictionary from https://github.com/google/fuzzing/blob/master/dictionaries/toml.dict

--- fuzz/oss-fuzz.sh
+++ fuzz/oss-fuzz.sh
@@ -3,4 +3,5 @@
 set -eux
 
 cargo fuzz build -O
+wget https://raw.githubusercontent.com/google/fuzzing/master/dictionaries/toml.dict -O $OUT/fuzz_from_to_string.dict
 cp fuzz/target/x86_64-unknown-linux-gnu/release/fuzz_from_to_string $OUT/fuzz_from_to_string
