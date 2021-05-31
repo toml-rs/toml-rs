21a212d3ebe1050cc3f623dda958a9554f4599fc
Evgeny Vereshchagin <evvers@ya.ru>
2021-05-31 17:19:36 +0000

    turn the tests into a seed corpus

--- fuzz/oss-fuzz.sh
+++ fuzz/oss-fuzz.sh
@@ -3,5 +3,6 @@
 set -eux
 
 cargo fuzz build -O
-wget https://raw.githubusercontent.com/google/fuzzing/master/dictionaries/toml.dict -O $OUT/fuzz_from_to_string.dict
 cp fuzz/target/x86_64-unknown-linux-gnu/release/fuzz_from_to_string $OUT/fuzz_from_to_string
+wget https://raw.githubusercontent.com/google/fuzzing/master/dictionaries/toml.dict -O $OUT/fuzz_from_to_string.dict
+zip -r $OUT/fuzz_from_to_string_seed_corpus.zip test-suite/tests
