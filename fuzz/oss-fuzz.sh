72b7aa01b03c04346b5284e4cf952ef8d032b868
Evgeny Vereshchagin <evvers@ya.ru>
2021-06-01 02:41:22 +0000

    oss-fuzz: pass --verbose to cargo
    It should make it easier to track down issues
    like https://github.com/google/oss-fuzz/pull/5865#issuecomment-852685588

--- fuzz/oss-fuzz.sh
+++ fuzz/oss-fuzz.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-cargo fuzz build -O
+cargo fuzz build -O --verbose
 cp fuzz/target/x86_64-unknown-linux-gnu/release/fuzz_from_to_string $OUT/fuzz_from_to_string
 wget https://raw.githubusercontent.com/google/fuzzing/master/dictionaries/toml.dict -O $OUT/fuzz_from_to_string.dict
 zip -r $OUT/fuzz_from_to_string_seed_corpus.zip test-suite/tests
