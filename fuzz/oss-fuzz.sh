da4b1ca8529e8ee52d247762f034ba4a72529222
Evgeny Vereshchagin <evvers@ya.ru>
2021-06-01 13:00:46 +0000

    oss-fuzz: allow switching between "dev" and "release" mode easily
    Mostly to be able to experiment with the fuzz target by injecting
    trivial bugs catchable by ASan to make sure that they aren't
    compiled out.
    
    It should help to track down weird issues like
    https://github.com/google/oss-fuzz/pull/5865#issuecomment-852685588

--- fuzz/oss-fuzz.sh
+++ fuzz/oss-fuzz.sh
@@ -2,7 +2,14 @@
 
 set -eux
 
-cargo fuzz build -O --verbose
-cp fuzz/target/x86_64-unknown-linux-gnu/release/fuzz_from_to_string $OUT/fuzz_from_to_string
+build_type=${1:-"release"}
+build_args="--release"
+if [[ "$build_type" =~ "dev" ]]; then
+    build_type="debug"
+    build_args="--dev"
+fi
+
+cargo fuzz build $build_args --verbose
+cp "fuzz/target/x86_64-unknown-linux-gnu/$build_type/fuzz_from_to_string" $OUT/fuzz_from_to_string
 wget https://raw.githubusercontent.com/google/fuzzing/master/dictionaries/toml.dict -O $OUT/fuzz_from_to_string.dict
 zip -r $OUT/fuzz_from_to_string_seed_corpus.zip test-suite/tests
